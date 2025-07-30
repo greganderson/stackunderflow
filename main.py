a30c660d7913243feeac85272033d6e5bbd80098
Greg Anderson <greg.anderson.cs@gmail.com>
2025-07-30 10:45:43 -0600

    Add database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import select, Session
 
+from database import get_db
 from models import Question, Answer
 from schemas import CreateQuestionRequest
 
@@ -13,42 +15,21 @@ app.add_middleware(
     allow_methods=["*"],
 )
 
-questions: list[Question] = [
-    Question(
-        title="How do you make a for loop?",
-        question="I've been trying to make a for loop, but I just can't figure it out. Anyone have any ideas?",
-        topic="Python",
-        likes=3,
-        user="Tony",
-        answers=[
-            Answer(
-                response="Why would you even want to do it that way?",
-                likes=2,
-                answered=False,
-                user="Treyson",
-            ),
-            Answer(
-                response="git gud",
-                likes=6,
-                answered=False,
-                user="Brandon",
-            ),
-            Answer(
-                response="It should look something like this: for i in range(10):...",
-                likes=47,
-                answered=True,
-                user="Hannah",
-            ),
-        ],
-    )
-]
-
 @app.get("/questions")
-async def get_questions() -> list[Question]:
-    return questions
+async def get_questions(db: Session = Depends(get_db)) -> list[Question]:
+    return db.exec(select(Question)).all()
+
+@app.get("/questions/{question_id}")
+async def get_question(question_id: int, db: Session = Depends(get_db)) -> Question:
+    question: Question | None = db.get(Question, question_id)
+    if question is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Question with ID {question_id} not found.")
+    return question
 
 @app.post("/questions", status_code=status.HTTP_201_CREATED)
-async def create_question(create_question_request: CreateQuestionRequest) -> Question:
+async def create_question(create_question_request: CreateQuestionRequest, db: Session = Depends(get_db)) -> int:
     question: Question = Question(**create_question_request.model_dump())
-    questions.append(question)
-    return question
+    db.add(question)
+    db.commit()
+    db.refresh(question)
+    return question.id
--- models.py
+++ models.py
@@ -1,16 +1,20 @@
-from pydantic import BaseModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
-class Answer(BaseModel):
+class Answer(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
     response: str
     likes: int
     answered: bool
     user: str
+    question_id: int = Field(foreign_key="question.id")
+    question: "Question" = Relationship(back_populates="answers")
 
-class Question(BaseModel):
+class Question(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
     title: str
     question: str
     topic: str | None
     likes: int = 0
     user: str
-    answers: list[Answer] = []
+    answers: list[Answer] = Relationship(back_populates="question")
